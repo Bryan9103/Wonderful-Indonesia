375d5d6c65c7edf184531bac2d5edfec248e530a
B <bryanandersen03@gmail.com>
2023-12-03 10:13:22 +0800

    Initial Commit

---
+++ Wonderful Indonesia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wonderful Indonesia
+//
+//  Created by Bryan Andersen on 2023/12/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wonderful Indonesia/Wonderful_IndonesiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wonderful_IndonesiaApp.swift
+//  Wonderful Indonesia
+//
+//  Created by Bryan Andersen on 2023/12/3.
+//
+
+import SwiftUI
+
+@main
+struct Wonderful_IndonesiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
